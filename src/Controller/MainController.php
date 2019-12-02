4bfff7802f2bc47a796ed1d29500c71d00c77212
Tomáš Zálešák <xzales13@vutbr.cz>
2019-12-02 22:05:39 +0100

    home nadchazejici turnjae

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Turnaj;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -11,9 +15,18 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="app_main")
      */
-    public function index()
+    public function index(EntityManagerInterface $em, Request $request, PaginatorInterface $paginator)
     {
+        $turnaje = $em->getRepository(Turnaj::class)->findAll();
+
+        $pagination = $paginator->paginate(
+            $turnaje, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            10/*limit per page*/
+        );
+
         return $this->render('main/main.html.twig', [
+            'turnaje' => $pagination
         ]);
     }
 }
